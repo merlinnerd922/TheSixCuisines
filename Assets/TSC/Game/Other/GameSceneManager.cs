d7f7f2179b57bd56093f0258e34e6384ae50208a
merlinnerd922 <richardb.chen@utoronto.ca>
2020-05-30 22:59:33 -0400

    Saving

--- Assets/TSC/Game/Other/GameSceneManager.cs
+++ Assets/TSC/Game/Other/GameSceneManager.cs
@@ -223,7 +223,7 @@ public class GameSceneManager : MonoBehaviour
     /// <summary>
     /// Create and load a brand new game state into the current scene.
     /// </summary>
-    /// <param name="level">TODO</param>
+    /// <param name="level">The level to load.</param>
     internal void LoadNewGameFromNewGameState(Level level)
     {
         // Create a new game state.
